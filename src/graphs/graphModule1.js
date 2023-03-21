5317a373637afa68e2e456dc17cf905897b3028b
megharajmk <127726310+megharajmk@users.noreply.github.com>
2023-03-21 18:47:31 +0530

    Update graphModule1.js

--- src/graphs/graphModule1.js
+++ src/graphs/graphModule1.js
@@ -101,7 +101,7 @@ export const GraphsModule1 = () => {
       <div className="mainWindow">
         <div className="container">
           <div style={{ margin: "10px" }}>
-            <h2 className="header">Engine 3 Reports</h2>
+            <h2 className="header">Engine 1 Statistics</h2>
           </div>
         </div>
         <div className="mainDiv">
